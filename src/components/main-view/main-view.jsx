980322b0d265f061f5e5ef9f5dc82e4bd175482f
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-10 18:21:29 +0100

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,43 +11,48 @@ export class MainView extends React.Component {
                 super();
                 // initializing state with starting values
                 this.state = {
-                        movies: [
-                                {
-                                        _id: 1,
-                                        Title: 'Black Panther',
-                                        Description:
-                                                "After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king - and as Black Panther - gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.",
-                                        ImagePath: 'https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/200x0/v2/https://resizing.flixster.com/KBlur3LaA-y1U1yt6_Y2uO25ozA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzMxOGI1YzBhLWMyMjEtNGUxMS1iM2Q0LWQ4OGMyYzQyZjQyYS53ZWJw',
-                                },
-                                {
-                                        _id: 2,
-                                        Title: 'The Big Sick',
-                                        Description:
-                                                'Kumail is a Pakistani comic, who meets an American graduate student named Emily at one of his stand-up shows. As their relationship blossoms, he soon becomes worried about what his traditional Muslim parents will think of her. When Emily suddenly comes down with an illness that leaves her in a coma, Kumail finds himself developing a bond with her deeply concerned mother and father.',
-                                        ImagePath: 'https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/200x0/v2/https://resizing.flixster.com/hcSU__fyxWKySypL-9PjigD5Vc0=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2MzMjVhNWUxLWRmOGMtNGFiNS1hMTg2LTUxMTdmYjFkNjJkZi53ZWJw',
-                                },
-                                {
-                                        _id: 3,
-                                        Title: 'Get Out',
-                                        Description:
-                                                "Now that Chris and his girlfriend Rose have reached the meet-the-parents milestone of dating, she invites him for a weekend getaway with Missy and Dean. At first, Chris reads the family's overly accommodating behavior as nervous attempts to deal with their daughter's interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries leads him to a truth that he never could have imagined.",
-                                        ImagePath: 'https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/200x0/v2/https://resizing.flixster.com/6bGQjhmuW9IZuzfpCKoO5FgdkYc=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzNiZGVhNTVmLWQzOGUtNGMwZC1hYmZjLTIxZWE5NThkMTdjNS53ZWJw',
-                                },
-                        ],
+                        movies: [],
                         selectedMovie: null,
+                        user: null,
                 };
         }
 
+        componentDidMount() {
+                axios.get('https://mequal.herokuapp.com/movies')
+                        .then((response) => {
+                                this.setState({
+                                        movies: response.data,
+                                });
+                        })
+                        .catch((error) => {
+                                console.log(error);
+                        });
+        }
+
+        /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
         setSelectedMovie(newSelectedMovie) {
                 this.setState({ selectedMovie: newSelectedMovie });
         }
 
+        /* When a user successfully logs in, this function updates the `user` property in state to that particular user */
+        onLoggedIn(user) {
+                this.setState({
+                        user,
+                });
+        }
+
         render() {
                 const { movies, selectedMovie } = this.state;
-                if (movies.length === 0) return <div className="main-view">The list is empty</div>;
+
+                /* If there's no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
+                // if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+                /* Before the movies have been loaded */
+                if (movies.length === 0) return <div className="main-view" />;
 
                 return (
                         <div className="main-view">
+                                {/* If the state of `selectedMovie`is not null, than selected movie will be returned, otherwise all movies will be returned */}
                                 {selectedMovie ? (
                                         <MovieView
                                                 movie={selectedMovie}
@@ -57,8 +65,8 @@ export class MainView extends React.Component {
                                                 <MovieCard
                                                         key={movie._id}
                                                         movieData={movie}
-                                                        onMovieClick={(movie) => {
-                                                                this.setSelectedMovie(movie);
+                                                        onMovieClick={(newSelectedMovie) => {
+                                                                this.setSelectedMovie(newSelectedMovie);
                                                         }}
                                                 />
                                         ))
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
         render() {
+                <button type="button">Registration</button>;
+
                 const { movieData, onMovieClick } = this.props;
                 return (
                         <div
@@ -15,3 +18,16 @@ export class MovieCard extends React.Component {
                 );
         }
 }
+
+MovieCard.propTypes = {
+        movieData: PropTypes.shape({
+                Title: PropTypes.string.isRequired,
+                Year: PropTypes.number.isRequired,
+                ImageUrl: PropTypes.string.isRequired,
+                Featured: PropTypes.bool.isRequired,
+                Genres: PropTypes.array.isRequired,
+                Actors: PropTypes.array.isRequired,
+                Directors: PropTypes.array.isRequired,
+        }).isRequired,
+        onMovieClick: PropTypes.func.isRequired,
+};
