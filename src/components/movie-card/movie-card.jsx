1480046f9f1d77d501e765bae51e705a34a8b903
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-11 15:16:41 +0100

    style: add Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
         render() {
                 const { movieData, onMovieClick } = this.props;
                 return (
-                        <div
-                                className="movie-card"
-                                onClick={() => {
-                                        onMovieClick(movieData);
-                                }}
-                        >
-                                {movieData.Title}
-                        </div>
+                        <>
+                                <Card>
+                                        <Card.Img variant="top" src={movieData.ImageUrl} />
+                                        <Card.Body>
+                                                <Card.Title>{movieData.Title}</Card.Title>
+                                                <Card.Text>{movieData.Description}</Card.Text>
+                                                <Button onClick={() => onMovieClick(movieData)} variant="link">
+                                                        Open
+                                                </Button>
+                                        </Card.Body>
+                                </Card>
+                                <div
+                                        className="movie-card"
+                                        onClick={() => {
+                                                onMovieClick(movieData);
+                                        }}
+                                >
+                                        {movieData.Title}
+                                </div>
+                        </>
                 );
         }
 }
@@ -23,6 +36,7 @@ MovieCard.propTypes = {
                 Year: PropTypes.number.isRequired,
                 ImageUrl: PropTypes.string.isRequired,
                 Featured: PropTypes.bool.isRequired,
+                Description: PropTypes.string,
                 Genres: PropTypes.array.isRequired,
                 Actors: PropTypes.array.isRequired,
                 Directors: PropTypes.array.isRequired,
