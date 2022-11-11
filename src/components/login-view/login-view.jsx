40047ddbd43f89751547033fdd5feeff47c2f4d0
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-11 16:16:24 +0100

    style: add form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
         const [username, setUsername] = useState('');
@@ -10,24 +12,23 @@ export function LoginView(props) {
                 console.log(username, password);
                 /* temporary till 3.6 */
                 /* Send a request to the server for authentication */
-                /* Then call props.onLoggedIn(username) */
                 props.onLoggedIn(username);
         };
 
         return (
-                <form>
-                        <label>
-                                Username:
-                                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                        </label>
-                        <label>
-                                Password:
-                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </label>
-                        <button type="submit" onClick={handleSubmit}>
+                <Form>
+                        <Form.Group controlId="formUsername">
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control type="text" onChange={(e) => setUsername(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>
                                 Submit
-                        </button>
-                </form>
+                        </Button>
+                </Form>
         );
 }
 
