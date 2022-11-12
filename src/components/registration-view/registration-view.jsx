ee5f941c31c664e9053eb80a1f8088a92521370a
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-12 19:58:21 +0100

    style: updated register form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button, Col } from 'react-bootstrap';
 
 export function RegistrationView(props) {
         const [username, setUsername] = useState('');
@@ -13,27 +14,59 @@ export function RegistrationView(props) {
         };
 
         return (
-                <form>
-                        <label>
-                                Username:
-                                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                        </label>
-                        <label>
-                                Password:
-                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </label>
-                        <label>
-                                Email:
-                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </label>
-                        <label>
-                                Birthday:
-                                <input type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
-                        </label>
-                        <button type="submit" onClick={handleSubmit}>
-                                Submit
-                        </button>
-                </form>
+                <div className="Register mt-5">
+                        <h1 className="display-1 text-center">Register</h1>
+                        <div>
+                                <h6 className="text-center">
+                                        You already have an account?
+                                        <div className="btn-link justify-content-center">Login</div>
+                                </h6>
+                        </div>
+                        <div className="row justify-content-center mt-5">
+                                <Col md={8} lg={6}>
+                                        <Form>
+                                                <Form.Group controlId="formUsername">
+                                                        <Form.Label>Username:</Form.Label>
+                                                        <Form.Control
+                                                                type="text"
+                                                                onChange={(e) => setUsername(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Form.Group controlId="formPassword">
+                                                        <Form.Label>Password:</Form.Label>
+                                                        <Form.Control
+                                                                type="password"
+                                                                onChange={(e) => setPassword(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Form.Group controlId="formEmail">
+                                                        <Form.Label>Email:</Form.Label>
+                                                        <Form.Control
+                                                                type="email"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Form.Group controlId="formEmail">
+                                                        <Form.Label>Birthday:</Form.Label>
+                                                        <Form.Control
+                                                                type="date"
+                                                                onChange={(e) => setBirthday(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Col>
+                                                        <Button
+                                                                className="btn mt-3"
+                                                                variant="dark"
+                                                                type="submit"
+                                                                onClick={handleSubmit}
+                                                        >
+                                                                Submit
+                                                        </Button>
+                                                </Col>
+                                        </Form>
+                                </Col>
+                        </div>
+                </div>
         );
 }
 
