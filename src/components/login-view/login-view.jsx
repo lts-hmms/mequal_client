ed9b2e32afc6dd69c4c322b70d9e7d188589a7cd
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-12 19:59:10 +0100

    style: updated login form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button, Col } from 'react-bootstrap';
 
 export function LoginView(props) {
         const [username, setUsername] = useState('');
@@ -16,19 +15,45 @@ export function LoginView(props) {
         };
 
         return (
-                <Form>
-                        <Form.Group controlId="formUsername">
-                                <Form.Label>Username:</Form.Label>
-                                <Form.Control type="text" onChange={(e) => setUsername(e.target.value)} />
-                        </Form.Group>
-                        <Form.Group controlId="formPassword">
-                                <Form.Label>Password:</Form.Label>
-                                <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" onClick={handleSubmit}>
-                                Submit
-                        </Button>
-                </Form>
+                <div className="Login mt-5">
+                        <h1 className="display-1 text-center">Login</h1>
+                        <div>
+                                <h6 className="text-center">
+                                        Not yet registered?
+                                        <div className="btn-link justify-content-center">Subscribe</div>
+                                </h6>
+                        </div>
+                        <div className="row justify-content-center mt-5">
+                                <Col md={8} lg={6}>
+                                        <Form>
+                                                <Form.Group controlId="formUsername">
+                                                        <Form.Label>Username:</Form.Label>
+                                                        <Form.Control
+                                                                type="text"
+                                                                onChange={(e) => setUsername(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Form.Group controlId="formPassword">
+                                                        <Form.Label>Password:</Form.Label>
+                                                        <Form.Control
+                                                                type="password"
+                                                                onChange={(e) => setPassword(e.target.value)}
+                                                        />
+                                                </Form.Group>
+                                                <Col>
+                                                        <Button
+                                                                className="btn mt-3"
+                                                                variant="dark"
+                                                                type="submit"
+                                                                onClick={handleSubmit}
+                                                        >
+                                                                Submit
+                                                        </Button>
+                                                </Col>
+                                        </Form>
+                                </Col>
+                        </div>
+                </div>
         );
 }
 
@@ -37,4 +62,5 @@ LoginView.propTypes = {
                 username: PropTypes.string.isRequired,
                 password: PropTypes.string.isRequired,
         }),
+        onLoggedIn: PropTypes.func.isRequired,
 };
