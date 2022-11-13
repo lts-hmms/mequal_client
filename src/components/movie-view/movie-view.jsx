b18c02c10b2487ac47eaeca4c88ebf05a9dfa2bc
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-13 19:13:54 +0100

    style update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,53 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
         render() {
                 const { movie, onBackClick } = this.props;
                 const { Genres, Directors, Actors } = movie;
                 return (
-                        <div className="movie-view">
-                                <div className="movie-poster">
-                                        <img alt="movie poster" style={{ width: '200px' }} src={movie.ImageUrl} />
-                                </div>
-                                <div className="movie-title">
-                                        <span className="label">Title: </span>
-                                        <span className="value">{movie.Title}</span>
-                                </div>
-                                <div className="movie-year">
-                                        <span className="label">Year: </span>
-                                        <span className="value">{movie.Year}</span>
-                                </div>
-                                <div className="movie-directors">
-                                        <span className="label">Directors: </span>
-                                        <span className="value">
-                                                {Directors.map(({ _id, Name }) => (
-                                                        <span key={Name}>{Name}, </span>
-                                                ))}
-                                        </span>
-                                </div>
-                                <div className="movie-description">
-                                        <span className="label">Description: </span>
-                                        <span className="value">{movie.Description}</span>
-                                </div>
-                                <div className="movie-genres">
-                                        <span className="label">Genres: </span>
-                                        <span className="value">
-                                                {Genres.map(({ _id, Name }) => (
-                                                        <span key={Name}>{Name}, </span>
-                                                ))}
-                                        </span>
-                                </div>
-                                <div className="movie-actors">
-                                        <span className="label">Actors: </span>
-                                        <span className="value">
-                                                {Actors.map(({ _id, Name }) => (
-                                                        <span key={Name}>{Name}, </span>
-                                                ))}
-                                        </span>
-                                </div>
-                                <button onClick={() => onBackClick(null)}>Go back!</button>
-                        </div>
+                        <Row>
+                                <Col md={8} className="movietext sm-12 my-auto">
+                                        <h2 className="movie-title mt-5">
+                                                <span className="value">{movie.Title}</span>
+                                        </h2>
+                                        <h5 className="movie-year">
+                                                <span className="value">{movie.Year}</span>
+                                        </h5>
+                                        <div className="movie-directors">
+                                                <span className="label">Directors: </span>
+                                                <span className="value">
+                                                        {Directors.map(({ _id, Name }) => (
+                                                                <span key={Name}>{Name}, </span>
+                                                        ))}
+                                                </span>
+                                        </div>
+                                        <div className="movie-description">
+                                                <span className="label">Description: </span>
+                                                <span className="value">{movie.Description}</span>
+                                        </div>
+                                        <div className="movie-genres">
+                                                <span className="label">Genres: </span>
+                                                <span className="value">
+                                                        {Genres.map(({ Name }) => (
+                                                                <span key={Name}>{Name}, </span>
+                                                        ))}
+                                                </span>
+                                        </div>
+                                        <div className="movie-actors">
+                                                <span className="label">Actors: </span>
+                                                <span className="value">
+                                                        {Actors.map(({ Name }) => (
+                                                                <span key={Name}>{Name}, </span>
+                                                        ))}
+                                                </span>
+                                        </div>
+                                        <Button variant="dark" className="btn mt-3" onClick={() => onBackClick(null)}>
+                                                Go back!
+                                        </Button>
+                                </Col>
+                                <Col md={4}>
+                                        <div className="movie-poster mt-5">
+                                                <img
+                                                        alt="movie poster"
+                                                        style={{ width: '125%' }}
+                                                        src={movie.ImageUrl}
+                                                />
+                                        </div>
+                                </Col>
+                        </Row>
                 );
         }
 }
