93387b15e87ce606b54c9db6eb900af1eb7f5091
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-14 15:53:05 +0100

    feat: Client side authetication & authorization

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,66 +1,67 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 export function LoginView(props) {
-        const [username, setUsername] = useState('');
-        const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-        const handleSubmit = (e) => {
-                e.preventDefault();
-                console.log(username, password);
-                /* temporary till 3.6 */
-                /* Send a request to the server for authentication */
-                props.onLoggedIn(username);
-        };
+    // post request to login endpoint of API using Axios
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /* Send a request to the server for authentication */
+        axios
+            .post('https://mequal.herokuapp.com/login', {
+                Username: username,
+                Password: password,
+            })
+            // backend sends back token & username, they go to onLoggedIn function
+            .then((response) => {
+                const { data } = response;
+                props.onLoggedIn(data);
+            })
+            .catch((e) => {
+                console.log('no such user');
+            });
+    };
 
-        return (
-                <div className="Login mt-5">
-                        <h1 className="display-1 text-center">Login</h1>
-                        <div>
-                                <h6 className="text-center">
-                                        Not yet registered?
-                                        <div className="btn-link justify-content-center">Subscribe</div>
-                                </h6>
-                        </div>
-                        <div className="row justify-content-center mt-5">
-                                <Col md={8} lg={6}>
-                                        <Form>
-                                                <Form.Group controlId="formUsername">
-                                                        <Form.Label>Username:</Form.Label>
-                                                        <Form.Control
-                                                                type="text"
-                                                                onChange={(e) => setUsername(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Form.Group controlId="formPassword">
-                                                        <Form.Label>Password:</Form.Label>
-                                                        <Form.Control
-                                                                type="password"
-                                                                onChange={(e) => setPassword(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Col>
-                                                        <Button
-                                                                className="btn mt-3"
-                                                                variant="dark"
-                                                                type="submit"
-                                                                onClick={handleSubmit}
-                                                        >
-                                                                Submit
-                                                        </Button>
-                                                </Col>
-                                        </Form>
-                                </Col>
-                        </div>
-                </div>
-        );
+    return (
+        <div className="Login mt-5">
+            <h1 className="display-1 text-center">Login</h1>
+            <div>
+                <h6 className="text-center">
+                    Not yet registered?
+                    <div className="btn-link justify-content-center">Subscribe</div>
+                </h6>
+            </div>
+            <div className="row justify-content-center mt-5">
+                <Col md={8} lg={6}>
+                    <Form>
+                        <Form.Group controlId="formUsername">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type="text" onChange={(e) => setUsername(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <Col>
+                            <Button className="btn mt-3" variant="dark" type="submit" onClick={handleSubmit}>
+                                Submit
+                            </Button>
+                        </Col>
+                    </Form>
+                </Col>
+            </div>
+        </div>
+    );
 }
 
 LoginView.propTypes = {
-        user: PropTypes.shape({
-                username: PropTypes.string.isRequired,
-                password: PropTypes.string.isRequired,
-        }),
-        onLoggedIn: PropTypes.func.isRequired,
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired,
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
 };
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -9,101 +9,135 @@ import mequalLogo from '../mequalLogo.png';
 import './main-view.scss';
 
 export class MainView extends React.Component {
-        constructor() {
-                // 'registers' class MainView as a React Component
-                super();
-                // initializing state with starting values
-                this.state = {
-                        movies: [],
-                        selectedMovie: null,
-                        user: null,
-                };
-        }
+    constructor() {
+        // 'registers' class MainView as a React Component
+        super();
+        // initializing state with starting values
+        this.state = {
+            movies: [],
+            selectedMovie: null,
+            user: null,
+        };
+    }
 
-        componentDidMount() {
-                axios.get('https://mequal.herokuapp.com/movies')
-                        .then((response) => {
-                                this.setState({
-                                        movies: response.data,
-                                });
-                        })
-                        .catch((error) => {
-                                console.log(error);
-                        });
+    /* retrieves information from local storage and checks if user is logged in, if yes GET request is made to movies endpoint by calling getMovies method */
+    componentDidMount() {
+        const accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user'),
+            });
+            this.getMovies(accessToken);
         }
+    }
 
-        /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
-        setSelectedMovie(newSelectedMovie) {
-                this.setState({ selectedMovie: newSelectedMovie });
-        }
+    /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({ selectedMovie: newSelectedMovie });
+    }
 
-        /* When a user successfully logs in, this function updates the `user` property in state to that particular user */
-        onLoggedIn(user) {
+    /* When a user successfully logs in, this function updates the `user` property in state and main-view is rendered again */
+    /* authentication data is saved to localStorage */
+    onLoggedIn(authData) {
+        console.log(authData);
+        this.setState({
+            user: authData.user.Username,
+        });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        /* the moment a user logs in, GET request to 'movies endpoint */
+        this.getMovies(authData.token);
+    }
+
+    getMovies(token) {
+        axios
+            .get('https://mequal.herokuapp.com/movies', { headers: { Authorization: `Bearer ${token}` } })
+            .then((response) => {
+                // Assign the result of the state
                 this.setState({
-                        user,
+                    movies: response.data,
                 });
-        }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-        render() {
-                const { movies, selectedMovie, user } = this.state;
+    onLoggedOut() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.setState({
+            user: null,
+        });
+    }
 
-                /* If there's no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
-                if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+    render() {
+        const { movies, selectedMovie, user } = this.state;
 
-                /* Before the movies have been loaded */
-                if (movies.length === 0) return <div className="main-view" />;
+        /* If there's no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
+        if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
-                return (
-                        <div>
-                                <NavbarView />
-                                {/* If the state of `selectedMovie`is not null, than selected movie will be returned, otherwise all movies will be returned */}
-                                {selectedMovie ? (
-                                        <Container>
-                                                <Row>
-                                                        <MovieView
-                                                                className="movie-view"
-                                                                movie={selectedMovie}
-                                                                onBackClick={(newSelectedMovie) => {
-                                                                        this.setSelectedMovie(newSelectedMovie);
-                                                                }}
-                                                        />
-                                                </Row>
-                                        </Container>
-                                ) : (
-                                        <Container>
-                                                <Row className="row justify-content-center mt-5">
-                                                        <Col md={3}>
-                                                                <img
-                                                                        className="logo "
-                                                                        src={mequalLogo}
-                                                                        alt="mequal logo"
-                                                                        style={{ width: '100%' }}
-                                                                />
-                                                        </Col>
-                                                </Row>
-                                                <Row className="row">
-                                                        {movies.map((movie) => (
-                                                                <Col md={4} key={movie._id}>
-                                                                        <div className="movie-cards mt-5">
-                                                                                <MovieCard
-                                                                                        movieData={movie}
-                                                                                        onMovieClick={(
-                                                                                                newSelectedMovie
-                                                                                        ) => {
-                                                                                                this.setSelectedMovie(
-                                                                                                        newSelectedMovie
-                                                                                                );
-                                                                                        }}
-                                                                                />
-                                                                        </div>
-                                                                </Col>
-                                                        ))}
-                                                </Row>
-                                        </Container>
-                                )}
-                        </div>
-                );
-        }
+        /* Before the movies have been loaded */
+        if (movies.length === 0) return <div className="main-view" />;
+
+        return (
+            <div>
+                <NavbarView />
+                {/* If the state of `selectedMovie`is not null, than selected movie will be returned, otherwise all movies will be returned */}
+                {selectedMovie ? (
+                    <Container>
+                        <Row>
+                            <MovieView
+                                className="movie-view"
+                                movie={selectedMovie}
+                                onBackClick={(newSelectedMovie) => {
+                                    this.setSelectedMovie(newSelectedMovie);
+                                }}
+                            />
+                        </Row>
+                    </Container>
+                ) : (
+                    <Container>
+                        <Row className="row">
+                            <Col md={5}>
+                                <Button
+                                    className="btn mt-3"
+                                    variant="dark"
+                                    type="submit"
+                                    onClick={() => {
+                                        this.onLoggedOut();
+                                    }}
+                                >
+                                    Logout
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Row className="row justify-content-center mt-5">
+                            <Col md={3}>
+                                <img className="logo " src={mequalLogo} alt="mequal logo" style={{ width: '100%' }} />
+                            </Col>
+                        </Row>
+
+                        <Row className="row">
+                            {movies.map((movie) => (
+                                <Col md={4} key={movie._id}>
+                                    <div className="movie-cards mt-5">
+                                        <MovieCard
+                                            movieData={movie}
+                                            onMovieClick={(newSelectedMovie) => {
+                                                this.setSelectedMovie(newSelectedMovie);
+                                            }}
+                                        />
+                                    </div>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                )}
+            </div>
+        );
+    }
 }
 
 export default MainView;
