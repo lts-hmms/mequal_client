98ca17880e6f99c87b0f6e03d36a1c366e59d76c
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-14 18:33:34 +0100

    feat: client side authenti & autho,register&login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -6,24 +6,50 @@ import axios from 'axios';
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    // hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    // validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username required');
+            isReq = false;
+        } else if (username.length < 3) {
+            setUsernameErr('Username has at least 3 characters');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPasswordErr('Password has at least 6 characters');
+            isReq = false;
+        }
+        return isReq;
+    };
 
     // post request to login endpoint of API using Axios
     const handleSubmit = (e) => {
         e.preventDefault();
-        /* Send a request to the server for authentication */
-        axios
-            .post('https://mequal.herokuapp.com/login', {
-                Username: username,
-                Password: password,
-            })
-            // backend sends back token & username, they go to onLoggedIn function
-            .then((response) => {
-                const { data } = response;
-                props.onLoggedIn(data);
-            })
-            .catch((e) => {
-                console.log('no such user');
-            });
+        const isReq = validate();
+        if (isReq) {
+            /* Send a request to the server for authentication */
+            axios
+                .post('https://mequal.herokuapp.com/login', {
+                    Username: username,
+                    Password: password,
+                })
+                // backend sends back token & username, they go to onLoggedIn function
+                .then((response) => {
+                    const { data } = response;
+                    props.onLoggedIn(data);
+                })
+                .catch((e) => {
+                    console.log('no such user');
+                });
+        }
     };
 
     return (
@@ -39,12 +65,25 @@ export function LoginView(props) {
                 <Col md={8} lg={6}>
                     <Form>
                         <Form.Group controlId="formUsername">
-                            <Form.Label>Username:</Form.Label>
-                            <Form.Control type="text" onChange={(e) => setUsername(e.target.value)} />
+                            <Form.Label />
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            {/* display validation error */}
+                            {usernameErr && <p>{usernameErr}</p>}
                         </Form.Group>
                         <Form.Group controlId="formPassword">
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control type="password" onChange={(e) => setPassword(e.target.value)} />
+                            <Form.Label />
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            {passwordErr && <p>{passwordErr}</p>}
                         </Form.Group>
                         <Col>
                             <Button className="btn mt-3" variant="dark" type="submit" onClick={handleSubmit}>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,80 +1,147 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 export function RegistrationView(props) {
-        const [username, setUsername] = useState('');
-        const [password, setPassword] = useState('');
-        const [email, setEmail] = useState[''];
-        const [birthday, setBirthday] = useState[''];
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState[''];
+    const [birthday, setBirthday] = useState[''];
+    // hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+    const [emailErr, setEmailErr] = useState[''];
+    const [birthdayErr, setBirthdayErr] = useState[''];
 
-        const handleSubmit = (e) => {
-                e.preventDefault();
-                console.log(username, password, email, birthday);
-        };
+    // validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username required');
+            isReq = false;
+        } else if (username.length < 3) {
+            setUsernameErr('Give me at least 3 characters please');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPasswordErr('Give me at least 6 characters please');
+            isReq = false;
+        }
+        if (!email) {
+            setEmailErr('Email required');
+            isReq = false;
+        } else if (email.indexOf('@') === -1 && email.indexOf('.') === -1) {
+            setPasswordErr("This doesn't look like an email to me ");
+            isReq = false;
+        }
+        if (!birthday) {
+            setBirthdayErr('Birthday required');
+            isReq = false;
+        }
+        return isReq;
+    };
 
-        return (
-                <div className="Register mt-5">
-                        <h1 className="display-1 text-center">Register</h1>
-                        <div>
-                                <h6 className="text-center">
-                                        You already have an account?
-                                        <div className="btn-link justify-content-center">Login</div>
-                                </h6>
-                        </div>
-                        <div className="row justify-content-center mt-5">
-                                <Col md={8} lg={6}>
-                                        <Form>
-                                                <Form.Group controlId="formUsername">
-                                                        <Form.Label>Username:</Form.Label>
-                                                        <Form.Control
-                                                                type="text"
-                                                                onChange={(e) => setUsername(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Form.Group controlId="formPassword">
-                                                        <Form.Label>Password:</Form.Label>
-                                                        <Form.Control
-                                                                type="password"
-                                                                onChange={(e) => setPassword(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Form.Group controlId="formEmail">
-                                                        <Form.Label>Email:</Form.Label>
-                                                        <Form.Control
-                                                                type="email"
-                                                                onChange={(e) => setEmail(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Form.Group controlId="formEmail">
-                                                        <Form.Label>Birthday:</Form.Label>
-                                                        <Form.Control
-                                                                type="date"
-                                                                onChange={(e) => setBirthday(e.target.value)}
-                                                        />
-                                                </Form.Group>
-                                                <Col>
-                                                        <Button
-                                                                className="btn mt-3"
-                                                                variant="dark"
-                                                                type="submit"
-                                                                onClick={handleSubmit}
-                                                        >
-                                                                Submit
-                                                        </Button>
-                                                </Col>
-                                        </Form>
-                                </Col>
-                        </div>
-                </div>
-        );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isReq = validate();
+        if (isReq) {
+            /* send a post request to the server */
+            axios
+                .post('https://mequal.herokuapp.com/users', {
+                    Username: username,
+                    Password: password,
+                    Email: email,
+                    Birthday: birthday,
+                })
+                .then((response) => {
+                    const { data } = response;
+                    console.log(data);
+                    alert('Registration successful, please login.');
+                });
+            /* second argument opens page in current tab */
+            window
+                .open('/', '_self')
+                // needs response from server if username already exists
+                .catch((response) => {
+                    console.error(response);
+                    alert('unable to register, please try again');
+                });
+        }
+    };
+
+    return (
+        <div className="Register mt-5">
+            <h1 className="display-1 text-center">Register</h1>
+            <div>
+                <h6 className="text-center">
+                    You already have an account?
+                    <div className="btn-link justify-content-center">Login</div>
+                </h6>
+            </div>
+            <div className="row justify-content-center mt-5">
+                <Col md={8} lg={6}>
+                    <Form>
+                        <Form.Group controlId="formUsername">
+                            <Form.Label />
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            {usernameErr && <p>{usernameErr}</p>}
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label />
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            {passwordErr && <p>{passwordErr}</p>}
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label />
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            {emailErr && <p>{emailErr}</p>}
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label />
+                            <Form.Control
+                                type="date"
+                                placeholder="Enter birthday"
+                                value={birthday}
+                                onChange={(e) => setBirthday(e.target.value)}
+                            />
+                            {birthdayErr && <p>{birthdayErr}</p>}
+                        </Form.Group>
+                        <Col>
+                            <Button className="btn mt-3" variant="dark" type="submit" onClick={handleSubmit}>
+                                Submit
+                            </Button>
+                        </Col>
+                    </Form>
+                </Col>
+            </div>
+        </div>
+    );
 }
 
 RegistrationView.propTypes = {
-        registration: PropTypes.shape({
-                username: PropTypes.string.isRequired,
-                password: PropTypes.string.isRequired,
-                email: PropTypes.string.isRequired,
-                birthday: PropTypes.string.isRequired,
-        }),
+    registration: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        birthday: PropTypes.string.isRequired,
+    }),
 };
