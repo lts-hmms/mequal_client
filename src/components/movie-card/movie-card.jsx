5920917b02df9a1bfb6183cffe985126504abdf4
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-15 18:31:04 +0100

    feat: add links to genre, director, movieview

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,50 +4,57 @@ import { Button, Card, Badge, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export class MovieCard extends React.Component {
-    render() {
-        const { movieData } = this.props;
-        return (
-            <Card>
-                <Link to={`/movies/${movieData._id}`}>
-                    <Card.Img className="movie-im" variant="top" src={movieData.ImageUrl} />
-                </Link>
-                <Card.Body className="cardbody text-center">
-                    <Card.Text>
-                        {movieData.Genres.map((genre) => (
-                            <Badge pill bg="light" text="dark">
-                                {genre.Name}
-                            </Badge>
-                        ))}
-                    </Card.Text>
-                    <Card.Title className="movieTitle mt-4" onClick={() => onMovieClick(movieData)}>
-                        {movieData.Title}
-                    </Card.Title>
-                    <Card.Text>{movieData.Year}</Card.Text>
-                    <Card.Text>
-                        {movieData.Directors.map((director) => (
-                            <div>by {director.Name}</div>
-                        ))}
-                    </Card.Text>
+  render() {
+    const { movieData } = this.props;
 
-                    <Button className="btn mt-2 justify-content-center" variant="dark">
-                        +
-                    </Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+    return (
+      <Card>
+        <Link to={`/movies/${movieData._id}`}>
+          <Card.Img
+            className="movie-im"
+            variant="top"
+            src={movieData.ImageUrl}
+          />
+        </Link>
+        <Card.Body className="cardbody text-center">
+          <Card.Text>
+            {movieData.Genres.map((genre) => (
+              <Link to={`/genres/${movieData.Genres.Name}`}>
+                <Badge pill variant="link" bg="light" text="dark">
+                  {genre.Name}
+                </Badge>
+              </Link>
+            ))}
+          </Card.Text>
+          <Card.Title className="movieTitle mt-4">{movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Year}</Card.Text>
+          <Card.Text>
+            {movieData.Directors.map((director) => (
+              <Link to={`/directors/${movieData.Directors.Name}`}>
+                <div>by {director.Name}</div>
+              </Link>
+            ))}
+          </Card.Text>
+
+          <Button className="btn mt-2 justify-content-center" variant="dark">
+            +
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 MovieCard.propTypes = {
-    movieData: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Year: PropTypes.number.isRequired,
-        ImageUrl: PropTypes.string.isRequired,
-        Featured: PropTypes.bool.isRequired,
-        Description: PropTypes.string,
-        Genres: PropTypes.array.isRequired,
-        Actors: PropTypes.array.isRequired,
-        Directors: PropTypes.array.isRequired,
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired,
+  movieData: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.number.isRequired,
+    ImageUrl: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    Description: PropTypes.string,
+    Genres: PropTypes.array.isRequired,
+    Actors: PropTypes.array.isRequired,
+    Directors: PropTypes.array.isRequired,
+  }).isRequired,
 };
