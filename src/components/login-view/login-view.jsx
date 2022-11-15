e6578c58f0d7d91ff0bdcfc2cb76f6eb931e6b5e
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-15 18:22:53 +0100

    style: eslint changes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,103 +4,108 @@ import { Form, Button, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 export function LoginView(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    // hook for each input
-    const [usernameErr, setUsernameErr] = useState('');
-    const [passwordErr, setPasswordErr] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // hook for each input
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
 
-    // validate user inputs
-    const validate = () => {
-        let isReq = true;
-        if (!username) {
-            setUsernameErr('Username required');
-            isReq = false;
-        } else if (username.length < 3) {
-            setUsernameErr('Username has at least 3 characters');
-            isReq = false;
-        }
-        if (!password) {
-            setPasswordErr('Password required');
-            isReq = false;
-        } else if (password.length < 6) {
-            setPasswordErr('Password has at least 6 characters');
-            isReq = false;
-        }
-        return isReq;
-    };
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr('Username required');
+      isReq = false;
+    } else if (username.length < 3) {
+      setUsernameErr('Username has at least 3 characters');
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr('Password required');
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr('Password has at least 6 characters');
+      isReq = false;
+    }
+    return isReq;
+  };
 
-    // post request to login endpoint of API using Axios
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isReq = validate();
-        if (isReq) {
-            /* Send a request to the server for authentication */
-            axios
-                .post('https://mequal.herokuapp.com/login', {
-                    Username: username,
-                    Password: password,
-                })
-                // backend sends back token & username, they go to onLoggedIn function
-                .then((response) => {
-                    const { data } = response;
-                    props.onLoggedIn(data);
-                })
-                .catch((e) => {
-                    console.log('no such user');
-                });
-        }
-    };
+  // post request to login endpoint of API using Axios
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+    if (isReq) {
+      /* Send a request to the server for authentication */
+      axios
+        .post('https://mequal.herokuapp.com/login', {
+          Username: username,
+          Password: password,
+        })
+        // backend sends back token & username, they go to onLoggedIn function
+        .then((response) => {
+          const { data } = response;
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log('no such user');
+        });
+    }
+  };
 
-    return (
-        <div className="Login mt-5">
-            <h1 className="display-1 text-center">Login</h1>
-            <div>
-                <h6 className="text-center">
-                    Not yet registered?
-                    <div className="btn-link justify-content-center">Subscribe</div>
-                </h6>
-            </div>
-            <div className="row justify-content-center mt-5">
-                <Col md={8} lg={6}>
-                    <Form>
-                        <Form.Group controlId="formUsername">
-                            <Form.Label />
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                            {/* display validation error */}
-                            {usernameErr && <p>{usernameErr}</p>}
-                        </Form.Group>
-                        <Form.Group controlId="formPassword">
-                            <Form.Label />
-                            <Form.Control
-                                type="password"
-                                placeholder="Enter password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            {passwordErr && <p>{passwordErr}</p>}
-                        </Form.Group>
-                        <Col>
-                            <Button className="btn mt-3" variant="dark" type="submit" onClick={handleSubmit}>
-                                Submit
-                            </Button>
-                        </Col>
-                    </Form>
-                </Col>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Login mt-5">
+      <h1 className="display-1 text-center">Login</h1>
+      <div>
+        <h6 className="text-center">
+          Not yet registered?
+          <div className="btn-link justify-content-center">Subscribe</div>
+        </h6>
+      </div>
+      <div className="row justify-content-center mt-5">
+        <Col md={8} lg={6}>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label />
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {/* display validation error */}
+              {usernameErr && <p>{usernameErr}</p>}
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label />
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {passwordErr && <p>{passwordErr}</p>}
+            </Form.Group>
+            <Col>
+              <Button
+                className="btn mt-3"
+                variant="dark"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </Col>
+          </Form>
+        </Col>
+      </div>
+    </div>
+  );
 }
 
 LoginView.propTypes = {
-    user: PropTypes.shape({
-        username: PropTypes.string.isRequired,
-        password: PropTypes.string.isRequired,
-    }),
-    onLoggedIn: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
 };
