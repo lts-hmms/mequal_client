ad3e6d92da72b0da9e3f090a43dfe97bc125a543
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-15 18:20:00 +0100

    feat: add director-view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export class DirectorView extends React.Component {
+  return;
+
+  render() {
+    <Container>
+      <Row>
+        <p>Hello</p>
+      </Row>
+    </Container>;
+  }
+}
