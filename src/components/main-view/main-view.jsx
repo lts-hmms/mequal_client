39581fe31652b0394b74e261427e4636f3ce6b79
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-15 13:14:28 +0100

    build: add routing to moviecards and movieview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,143 +1,137 @@
 import React from 'react';
 import axios from 'axios';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { Row, Col, Container, Button } from 'react-bootstrap';
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { NavbarView } from '../navbar/navbar';
+
 import mequalLogo from '../mequalLogo.png';
 import './main-view.scss';
 
 export class MainView extends React.Component {
-    constructor() {
-        // 'registers' class MainView as a React Component
-        super();
-        // initializing state with starting values
-        this.state = {
-            movies: [],
-            selectedMovie: null,
-            user: null,
-        };
-    }
-
-    /* retrieves information from local storage and checks if user is logged in, if yes GET request is made to movies endpoint by calling getMovies method */
-    componentDidMount() {
-        const accessToken = localStorage.getItem('token');
-        if (accessToken !== null) {
-            this.setState({
-                user: localStorage.getItem('user'),
-            });
-            this.getMovies(accessToken);
+        constructor() {
+                // 'registers' class MainView as a React Component
+                super();
+                // initializing state with starting values
+                this.state = {
+                        movies: [],
+                        selectedMovie: null,
+                        user: null,
+                };
         }
-    }
 
-    /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
-    setSelectedMovie(newSelectedMovie) {
-        this.setState({ selectedMovie: newSelectedMovie });
-    }
+        /* retrieves information from local storage and checks if user is logged in, if yes GET request is made to movies endpoint by calling getMovies method */
+        componentDidMount() {
+                const accessToken = localStorage.getItem('token');
+                if (accessToken !== null) {
+                        this.setState({
+                                user: localStorage.getItem('user'),
+                        });
+                        this.getMovies(accessToken);
+                }
+        }
 
-    /* When a user successfully logs in, this function updates the `user` property in state and main-view is rendered again */
-    /* authentication data is saved to localStorage */
-    onLoggedIn(authData) {
-        console.log(authData);
-        this.setState({
-            user: authData.user.Username,
-        });
+        /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
+        setSelectedMovie(newSelectedMovie) {
+                this.setState({ selectedMovie: newSelectedMovie });
+        }
 
-        localStorage.setItem('token', authData.token);
-        localStorage.setItem('user', authData.user.Username);
-        /* the moment a user logs in, GET request to 'movies endpoint */
-        this.getMovies(authData.token);
-    }
-
-    getMovies(token) {
-        axios
-            .get('https://mequal.herokuapp.com/movies', { headers: { Authorization: `Bearer ${token}` } })
-            .then((response) => {
-                // Assign the result of the state
+        /* When a user successfully logs in, this function updates the `user` property in state and main-view is rendered again */
+        /* authentication data is saved to localStorage */
+        onLoggedIn(authData) {
+                console.log(authData);
                 this.setState({
-                    movies: response.data,
+                        user: authData.user.Username,
                 });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
 
-    onLoggedOut() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        this.setState({
-            user: null,
-        });
-    }
+                localStorage.setItem('token', authData.token);
+                localStorage.setItem('user', authData.user.Username);
+                /* the moment a user logs in, GET request to 'movies endpoint */
+                this.getMovies(authData.token);
+        }
 
-    render() {
-        const { movies, selectedMovie, user } = this.state;
+        getMovies(token) {
+                axios.get('https://mequal.herokuapp.com/movies', { headers: { Authorization: `Bearer ${token}` } })
+                        .then((response) => {
+                                // Assign the result of the state
+                                this.setState({
+                                        movies: response.data,
+                                });
+                        })
+                        .catch((error) => {
+                                console.log(error);
+                        });
+        }
 
-        /* If there's no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
-        if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+        onLoggedOut() {
+                localStorage.removeItem('token');
+                localStorage.removeItem('user');
+                this.setState({
+                        user: null,
+                });
+        }
 
-        /* Before the movies have been loaded */
-        if (movies.length === 0) return <div className="main-view" />;
+        render() {
+                const { movies, selectedMovie, user } = this.state;
 
-        return (
-            <div>
-                <NavbarView />
-                {/* If the state of `selectedMovie`is not null, than selected movie will be returned, otherwise all movies will be returned */}
-                {selectedMovie ? (
-                    <Container>
-                        <Row>
-                            <MovieView
-                                className="movie-view"
-                                movie={selectedMovie}
-                                onBackClick={(newSelectedMovie) => {
-                                    this.setSelectedMovie(newSelectedMovie);
-                                }}
-                            />
-                        </Row>
-                    </Container>
-                ) : (
-                    <Container>
-                        <Row className="row">
-                            <Col md={5}>
-                                <Button
-                                    className="btn mt-3"
-                                    variant="dark"
-                                    type="submit"
-                                    onClick={() => {
-                                        this.onLoggedOut();
-                                    }}
-                                >
-                                    Logout
-                                </Button>
-                            </Col>
-                        </Row>
-                        <Row className="row justify-content-center mt-5">
-                            <Col md={3}>
-                                <img className="logo " src={mequalLogo} alt="mequal logo" style={{ width: '100%' }} />
-                            </Col>
-                        </Row>
+                /* If there's no user, the LoginView is rendered. If there is a user logged in, the user details are passed as a prop to the LoginView */
+                if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
-                        <Row className="row">
-                            {movies.map((movie) => (
-                                <Col md={4} key={movie._id}>
-                                    <div className="movie-cards mt-5">
-                                        <MovieCard
-                                            movieData={movie}
-                                            onMovieClick={(newSelectedMovie) => {
-                                                this.setSelectedMovie(newSelectedMovie);
-                                            }}
-                                        />
-                                    </div>
-                                </Col>
-                            ))}
-                        </Row>
-                    </Container>
-                )}
-            </div>
-        );
-    }
+                /* Before the movies have been loaded */
+                if (movies.length === 0) return <div className="main-view" />;
+
+                return (
+                        <Router>
+                                <div>
+                                        <NavbarView />
+                                        {/* If the state of `selectedMovie`is not null, than selected movie will be returned, otherwise all movies will be returned */}
+                                        <Container>
+                                                <Row>
+                                                        <Route
+                                                                path="/movies/:movieId"
+                                                                render={({ match }) => (
+                                                                        <Col md={8}>
+                                                                                <MovieView
+                                                                                        movie={movies.find(
+                                                                                                (m) =>
+                                                                                                        m._id ===
+                                                                                                        match.params
+                                                                                                                .movieId
+                                                                                        )}
+                                                                                />
+                                                                        </Col>
+                                                                )}
+                                                        />
+                                                </Row>
+                                        </Container>
+                                        <Container>
+                                                <Row className="row">
+                                                        <Route
+                                                                exact
+                                                                path="/"
+                                                                render={() =>
+                                                                        movies.map((m) => (
+                                                                                <Col md={4} key={m._id}>
+                                                                                        <div className="movie-cards mt-5">
+                                                                                                <MovieCard
+                                                                                                        movieData={m}
+                                                                                                />
+                                                                                        </div>
+                                                                                </Col>
+                                                                        ))
+                                                                }
+                                                        />
+                                                </Row>
+                                        </Container>
+                                </div>
+                        </Router>
+                );
+        }
 }
 
 export default MainView;
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,55 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, Badge, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export class MovieCard extends React.Component {
-        render() {
-                const { movieData, onMovieClick } = this.props;
-                return (
-                        <Card>
-                                <Card.Img
-                                        className="movie-im"
-                                        variant="link"
-                                        src={movieData.ImageUrl}
-                                        onClick={() => onMovieClick(movieData)}
-                                />
-                                <Card.Body className="cardbody text-center">
-                                        <Card.Text>
-                                                {movieData.Genres.map((genre) => (
-                                                        <Badge pill bg="light" text="dark">
-                                                                {genre.Name}
-                                                        </Badge>
-                                                ))}
-                                        </Card.Text>
-                                        <Card.Title className="movieTitle mt-4" onClick={() => onMovieClick(movieData)}>
-                                                {movieData.Title}
-                                        </Card.Title>
-                                        <Card.Text>{movieData.Year}</Card.Text>
-                                        <Card.Text>
-                                                {movieData.Directors.map((director) => (
-                                                        <div>by {director.Name}</div>
-                                                ))}
-                                        </Card.Text>
+    render() {
+        const { movieData } = this.props;
+        return (
+            <Card>
+                <Link to={`/movies/${movieData._id}`}>
+                    <Card.Img className="movie-im" variant="top" src={movieData.ImageUrl} />
+                </Link>
+                <Card.Body className="cardbody text-center">
+                    <Card.Text>
+                        {movieData.Genres.map((genre) => (
+                            <Badge pill bg="light" text="dark">
+                                {genre.Name}
+                            </Badge>
+                        ))}
+                    </Card.Text>
+                    <Card.Title className="movieTitle mt-4" onClick={() => onMovieClick(movieData)}>
+                        {movieData.Title}
+                    </Card.Title>
+                    <Card.Text>{movieData.Year}</Card.Text>
+                    <Card.Text>
+                        {movieData.Directors.map((director) => (
+                            <div>by {director.Name}</div>
+                        ))}
+                    </Card.Text>
 
-                                        <Button className="btn mt-2 justify-content-center" variant="dark">
-                                                +
-                                        </Button>
-                                </Card.Body>
-                        </Card>
-                );
-        }
+                    <Button className="btn mt-2 justify-content-center" variant="dark">
+                        +
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 
 MovieCard.propTypes = {
-        movieData: PropTypes.shape({
-                Title: PropTypes.string.isRequired,
-                Year: PropTypes.number.isRequired,
-                ImageUrl: PropTypes.string.isRequired,
-                Featured: PropTypes.bool.isRequired,
-                Description: PropTypes.string,
-                Genres: PropTypes.array.isRequired,
-                Actors: PropTypes.array.isRequired,
-                Directors: PropTypes.array.isRequired,
-        }).isRequired,
-        onMovieClick: PropTypes.func.isRequired,
+    movieData: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Year: PropTypes.number.isRequired,
+        ImageUrl: PropTypes.string.isRequired,
+        Featured: PropTypes.bool.isRequired,
+        Description: PropTypes.string,
+        Genres: PropTypes.array.isRequired,
+        Actors: PropTypes.array.isRequired,
+        Directors: PropTypes.array.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
 };
