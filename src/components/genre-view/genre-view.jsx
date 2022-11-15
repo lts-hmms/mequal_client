360019ce710786acdcf869f5cca54271c13ccb2d
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-15 18:20:49 +0100

    feat: add genre-view

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export class GenreView extends React.Component {
+  render() {
+    <Container>
+      <Row>
+        <p>Hello</p>
+      </Row>
+    </Container>;
+  }
+}
