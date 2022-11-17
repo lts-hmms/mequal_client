edc7bd3090cf96b5e726c0aaf87ca92d3f1df6eb
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-17 09:19:01 +0100

    feat: remove dropdown,add login dependency

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown, Form } from 'react-bootstrap';
+import { Container, Nav, Navbar, Form, Button } from 'react-bootstrap';
 
-export function NavbarView() {
-        return (
-                <Navbar bg="dark" variant="dark" expand="lg">
-                        <Navbar.Brand href="#home">mequal</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="me-auto">
-                                        <Nav.Link href="#home">Home</Nav.Link>
-                                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                                <NavDropdown.Item href="#action/3.1">Genres</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.2">Directors</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.3">Actors</NavDropdown.Item>
-                                        </NavDropdown>
-                                </Nav>
-                                <Form className="d-flex">
-                                        <Form.Control
-                                                type="search"
-                                                placeholder="Search"
-                                                className="me-2"
-                                                aria-label="Search"
-                                        />
-                                </Form>
-                        </Navbar.Collapse>
-                </Navbar>
-        );
+export function NavbarView({ user }) {
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open('/', '_self');
+  };
+
+  const isAuth = () => {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+    if (localStorage.getItem('token')) {
+      return localStorage.getItem('token');
+    }
+    return false;
+  };
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">mequal</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
+          {isAuth() && <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>}
+          {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
+          {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavbarView;
