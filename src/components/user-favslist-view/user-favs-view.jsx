3133f33fc29dc5ffeb0e632d3bd5985186dc7e78
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-18 11:38:00 +0100

    feat: add user-favs-view

---
+++ src/components/user-favslist-view/user-favs-view.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card, Badge, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export class UserFavsView extends React.Component {
+  render() {
+    const { movieData } = this.props;
+
+    return <Container />;
+  }
+}
