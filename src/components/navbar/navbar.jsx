c85563acad4a2f60cc69c74c08fdd39b7cd0a2c7
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-22 13:37:57 +0100

    fix: deleted favs in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -19,12 +19,11 @@ export function NavbarView({ user }) {
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">mequal</Navbar.Brand>
+      <Navbar.Brand href="/">mequal</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
-          {isAuth() && <Nav.Link href={`/users/${user}/favs`}>Favs</Nav.Link>}
           {isAuth() && <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>}
           {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
           {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
