bc83ead462369a08ae2d0fa41231badf2573d769
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-22 13:39:03 +0100

    Feat: update user pw added after bug

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Button, Row, Col, Form, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Moment from 'react-moment';
+import { MovieCard } from '../movie-card/movie-card';
 
 export class ProfileView extends React.Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
     this.state = {
-      Username: null,
-      Password: null,
-      Email: null,
+      Password: '',
+      Email: '',
       Birthday: null,
-      Favs: [],
+      Favslist: [],
     };
   }
 
   componentDidMount() {
     this.getUser();
-    console.log(this.props);
+  }
+
+  validate() {
+    let isReq = true;
+    if (password && password.length < 6) {
+      setPasswordErr('Give me at least 6 characters please');
+      isReq = false;
+    }
+    if (email && email.indexOf('@') === -1 && email.indexOf('.') === -1) {
+      setEmailErr("This doesn't look like an email to me ");
+      isReq = false;
+    }
+    return isReq;
   }
 
   getUser() {
-    const username = localStorage.getItem('user');
     const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
     axios
       .get(`https://mequal.herokuapp.com/users/${username}`, {
         headers: { Authorization: `Bearer ${token}` },
       })
-      .then((response) => {
+      .then((res) => {
         this.setState({
-          Username: response.data.Username,
-          Password: response.data.Password,
-          Email: response.data.Email,
-          Birthday: response.data.Birthday,
-          Favs: response.data.Favslist,
+          Password: res.data.Password,
+          Email: res.data.Email,
+          Birthday: res.data.Birthday,
+          Favslist: res.data.Favslist,
         });
       })
       .catch((error) => {
@@ -44,25 +54,70 @@ export class ProfileView extends React.Component {
       });
   }
 
-  setUsername(input) {
-    this.Username = input;
+  deleteProfile(e) {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
+    axios
+      .delete(`https://mequal.herokuapp.com/users/${username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(() => {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        alert('Profile has been deleted.');
+        window.open('/', '_self');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 
-  setPassword(input) {
-    this.Password = input;
+  updateUser(e) {
+    e.preventDefault();
+    const username = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    axios
+      .patch(
+        `https://mequal.herokuapp.com/users/${username}`,
+        {
+          Email: this.state.Email,
+          ...(this.Password ? { Password: this.state.Password } : {}),
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((res) => {
+        console.log('response', res);
+        alert('Your profile is updated now.');
+        this.setState({
+          Password: res.data.Password,
+          Email: res.data.Email,
+        });
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
   }
 
-  setEmail(input) {
-    this.Email = input;
-  }
+  handleChangeEmail = (event) => {
+    this.setState({ Email: event.target.value });
+  };
 
-  setBirthday(input) {
-    this.Birthday = input;
-  }
+  handleChangePW = (event) => {
+    this.setState({ Password: event.target.value });
+  };
 
   render() {
-    const { user, Birthday } = this.props;
-    const { Password, Email } = this.state;
+    const { Birthday } = this.state;
+    // const { movies } = this.props;
+    const { user } = this.props;
+    const { Email } = this.state;
+    const { Password } = this.state;
+    const { Favslist } = this.state;
+    const FavsIds = Favslist.map((movie, key) => movie._id);
+    const favsIds = [];
 
     return (
       <div className="Profile mt-5">
@@ -70,34 +125,34 @@ export class ProfileView extends React.Component {
 
         <div className="row justify-content-center mt-5">
           <Col md={8} lg={6}>
+            <h5 className="Username">
+              <div className="value">Username:</div>
+              <div className="text-secondary">{user}</div>
+            </h5>
+            <h5 className="Birthday">
+              <div className="value">Birthday: </div>
+              <div className="text-secondary">
+                <Moment format="YYYY-MM-DD">{Birthday}</Moment>
+              </div>
+            </h5>
             <Form>
-              <h5 className="Username">
-                <div className="value">Username:</div>
-                <div className="text-secondary">{user}</div>
-              </h5>
-              <h5 className="Birthday">
-                <div className="value">Birthday: </div>
-                <div className="text-secondary">
-                  <Moment format="YYYY-MM-DD">{Birthday}</Moment>
-                </div>
-              </h5>
               <Form.Group controlId="formPassword">
                 <Form.Label className="h5">Password:</Form.Label>
                 <Form.Control
                   type="password"
                   placeholder="******"
                   value={this.Password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={this.handleChangePW}
                 />
-                {/* {passwordErr && <p>{passwordErr}</p>} */}
+
+                {/* {PasswordErr && <p>{PasswordErr}</p>} */}
               </Form.Group>
               <Form.Group controlId="formEmail">
                 <Form.Label className="h5">Email:</Form.Label>
                 <Form.Control
                   type="email"
                   placeholder={Email}
-                  value={this.Email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={this.handleChangeEmail}
                 />
                 {/* {emailErr && <p>{emailErr}</p>} */}
               </Form.Group>
@@ -106,54 +161,40 @@ export class ProfileView extends React.Component {
                   className="btn mt-3"
                   variant="dark"
                   type="submit"
-                  //   onClick={handleRegister}
+                  // eslint-disable-next-line react/jsx-no-bind
+                  onClick={this.updateUser.bind(this)}
                 >
-                  Submit
+                  update profile
                 </Button>
               </Col>
             </Form>
           </Col>
+          <Row className="mt-5">
+            <Link
+              className="h6 text-center btn-link justify-content-center"
+              onClick={this.deleteProfile}
+            >
+              Delete profile
+            </Link>
+          </Row>
+        </div>
+
+        <div className="Favslist row justify-content-center mt-5">
+          {Favslist.map((m) => {
+            const res = movies.filter((movie) => movie._id === m._id);
+            if (res.length > 0) {
+              return res.map((m) => (
+                <Col md={4} key={m._id}>
+                  <div className="movie-cards mt-5">
+                    <MovieCard movieData={m} />
+                  </div>
+                </Col>
+              ));
+            }
+          })}
         </div>
       </div>
     );
   }
 }
 export default ProfileView;
-
-// export function ProfileView(props) {
-//   const { user } = props;
-//   const token = localStorage.getItem('token');
-
-//    useEffect(() => {async function() {
-//       const res = await axios.get(`https://mequal.herokuapp.com/users/${user}`, {
-//         headers: { Authorization: `Bearer ${token}` },
-//       });
-//     }}, [])
-
-//   const [password, setPassword] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [birthday, setBirthday] = useState('');
-//   // hook for each input
-//   //   const [passwordErr, setPasswordErr] = useState('');
-//   //   const [emailErr, setEmailErr] = useState('');
-//   //   const [birthdayErr, setBirthdayErr] = useState('');
-
-//   const [isDisabled, setIsDisabled] = useState(false);
-
-//   const handleClick = () => {
-//     setIsDisabled(isDisabled);
-//   };
-
-//   useEffect(() => {
-//     async function fetchUser() {
-//       const response = await fetch(
-//         `https://mequal.herokuapp.com/users/${user}`,
-//         {
-//           headers: { Authorization: `Bearer ${token}` },
-//         }
-//       );
-//       console.log(response);
-//       setEmail(response.data);
-//     }
-//     fetchUser();
-//   }, []);
