c0a25ca6da6d3840a764fa605f2f844aa1ee6af1
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-27 17:11:23 +0100

    build: redux added

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
+
 import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that I need to bundle. `./index.scss`
 import './index.scss';
 
+const mequalStore = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class mequalApplication extends React.Component {
   render() {
     return (
-      <div>
-        <MainView />
-      </div>
+      // inside provider pass every comp that needs access to store
+      <Provider store={mequalStore}>
+        <div>
+          <MainView />
+        </div>
+      </Provider>
     );
   }
 }
