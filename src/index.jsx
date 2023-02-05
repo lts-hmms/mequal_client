d0695e68470df3c56f530d62bbc4bd58aada7e17
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-02-05 17:05:45 +0100

    feat: update to react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
-
 import MainView from './components/main-view/main-view';
-
-// Import statement to indicate that I need to bundle. `./index.scss`
 import './index.scss';
 
 const mequalStore = createStore(moviesApp, devToolsEnhancer());
 
-// Main component (will eventually use all the others)
-class mequalApplication extends React.Component {
-  render() {
-    return (
-      // inside provider pass every comp that needs access to store
-      <Provider store={mequalStore}>
-        <div>
-          <MainView />
-        </div>
-      </Provider>
-    );
-  }
-}
+const MequalApplication = () => (
+  <Provider store={mequalStore}>
+    <div>
+      <MainView />
+    </div>
+  </Provider>
+);
 
-// Finds the root of my app
-const container = document.getElementsByClassName('app-container')[0];
-
-// Tells React to render my app in the root DOM element
-ReactDom.render(React.createElement(mequalApplication), container);
+const container = document.getElementById('app-container');
+const root = createRoot(container);
+root.render(<MequalApplication />);
