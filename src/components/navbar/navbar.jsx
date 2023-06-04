1071887230d8623a2304ea774f35bf9594f4439e
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-06-04 20:55:02 +0200

    feat: fix navbar on top

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -22,7 +22,7 @@ export function NavbarView() {
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
         <Navbar.Brand as={Link} to="/">
           mequal
